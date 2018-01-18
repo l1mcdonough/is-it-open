594c77c4b4040556cd9cf43358ccdc9df909b134
Luke <l1mcdonough@bridgew.edu>
2018-01-18 18:22:46 -0500

    added bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
-
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Is It Open?"
+  end
   test "should get home" do
-    get static_pages_home_url
+    get "/"
     assert_response :success
+    assert_select "title", "Find stores open on holidays | #{@base_title}"
   end
 
   test "should get about" do
@@ -15,5 +18,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_contact_url
     assert_response :success
   end
+
 
 end
